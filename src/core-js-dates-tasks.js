1739f3bab8647744bf7394f281f6d0265511b871
Walle908 <walle0908@yandex.ru>
2026-01-05 15:04:17 +0400

    feat: add solution for dateToTimestamp, getTime, getDayName

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -17,8 +17,8 @@
  * '01 Jan 1970 00:00:00 UTC' => 0
  * '04 Dec 1995 00:12:00 UTC' => 818035920000
  */
-function dateToTimestamp(/* date */) {
-  throw new Error('Not implemented');
+function dateToTimestamp(date) {
+  return new Date(date).getTime();
 }
 
 /**
@@ -31,8 +31,16 @@ function dateToTimestamp(/* date */) {
  * Date(2023, 5, 1, 8, 20, 55) => '08:20:55'
  * Date(2015, 10, 20, 23, 15, 1) => '23:15:01'
  */
-function getTime(/* date */) {
-  throw new Error('Not implemented');
+function getTime(date) {
+  function format(item) {
+    return item.toString().length === 2 ? item : `0${item}`;
+  }
+
+  const hours = format(date.getHours());
+  const minutes = format(date.getMinutes());
+  const seconds = format(date.getSeconds());
+
+  return `${hours}:${minutes}:${seconds}`;
 }
 
 /**
@@ -46,8 +54,35 @@ function getTime(/* date */) {
  * '03 Dec 1995 00:12:00 UTC' => 'Sunday'
  * '2024-01-30T00:00:00.000Z' => 'Tuesday'
  */
-function getDayName(/* date */) {
-  throw new Error('Not implemented');
+function getDayName(date) {
+  const day = new Date(date).getUTCDay();
+  let result;
+  switch (day) {
+    case 0:
+      result = 'Sunday';
+      break;
+    case 1:
+      result = 'Monday';
+      break;
+    case 2:
+      result = 'Tuesday';
+      break;
+    case 3:
+      result = 'Wednesday';
+      break;
+    case 4:
+      result = 'Thursday';
+      break;
+    case 5:
+      result = 'Friday';
+      break;
+    case 6:
+      result = 'Saturday';
+      break;
+    default:
+      result = 'There is an error';
+  }
+  return result;
 }
 
 /**
